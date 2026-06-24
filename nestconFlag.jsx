3776c6b4317520ad99d96c2523560dd5dec98e76
TroyLynx <troylynx01@gmail.com>
2026-06-24 00:29:44 +0000

    nestonFlag created

---
+++ nestconFlag.jsx
@@ -0,0 +1,14 @@
+export default function FlagBanner() {
+    return (
+        <div style={{
+            backgroundColor: '#1f2937',
+            color: '#fff',
+            padding: '20px',
+            textAlign: 'center',
+            fontSize: '24px',
+            fontWeight: 'bold'
+        }}>
+            flag
+        </div>
+    );
+}
